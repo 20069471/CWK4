758f93596e090a7ecb352603d0a7af1a21287def
20069471 <154887261+20069471@users.noreply.github.com>
2024-04-26 18:57:56 +0100

    Add files via upload

---
+++ Teamwork.java
@@ -0,0 +1,54 @@
+package cwk4; 
+
+
+/**
+ * Details of your team
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Teamwork
+{
+    private String[] details = new String[12];
+    
+    public Teamwork()
+    {   // in each line replace the contents of the String 
+        // with the details of your team member
+        // Please list the member details alphabetically by surname 
+        // i.e. the surname of member1 should come alphabetically 
+        // before the surname of member 2...etc
+        details[0] = "53";
+        
+        details[1] = "Lagadapati ";
+        details[2] = "Vineeth";
+        details[3] = "20054702";
+
+        details[4] = "venkatesh";
+        details[5] = "Gangineni ";
+        details[6] = "20069471";
+
+        details[7] = " komatineni";
+        details[8] = "Revanth";
+        details[9] = "20055825";
+
+
+        details[10] = "Reddy govindu";
+        details[11] = "Sudharshan ";
+        details[12] = "21063934";
+
+    }
+    
+    public String[] getTeamDetails()
+    {
+        return details;
+    }
+    
+    public void displayDetails()
+    {
+        for(String temp:details)
+        {
+            System.out.println(temp.toString());
+        }
+    }
+}
+        
